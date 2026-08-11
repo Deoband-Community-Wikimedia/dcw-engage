b02ac965ea85072c42df02f37f34d69b5b0f28ce
zaidusyy <zaidsayyed07869@gmail.com>
2026-08-11 21:02:34 +0530

    style: fix action buttons (.btn, a.btn) styling and add asset cache-busting

--- views/admin/form_manager.php
+++ views/admin/form_manager.php
@@ -163,7 +163,7 @@ if (!empty($activeFieldFilters)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($form['title']) ?> - Form Manager</title>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/assets/css/admin.css">
+    <link rel="stylesheet" href="/assets/css/admin.css?v=<?= filemtime(__DIR__ . '/../../assets/css/admin.css') ?>">
 </head>
 
 <body>
